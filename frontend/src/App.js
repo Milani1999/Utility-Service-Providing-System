5c2ec96635866641bd57c5eff35e53073382d53a
Milani <milaniyoges@gmail.com>
2022-11-13 17:38:49 +0530

    add component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,3 @@
-// import { BrowserRouter, Route, Switch } from "react-router-dom";
-// import Dashboard from "./components/Dashboard";
-// import Login from "./components/Login";
-// import Navbar from "./components/Navbar";
-// import Register from "./components/Register";
-// import NavbarNew from './components/supportComponents/NavbarNew';
- 
-// function App() {
-//   return (
-//     <BrowserRouter>
-//     <NavbarNew />
-//       <Switch>
-//         <Route exact path="/">
-//           <Login/>
-//         </Route>
-//         <Route path="/register">
-//           <Register/>
-//         </Route>
-//         <Route path="/dashboard">
-//           <Navbar/>
-//           <Dashboard/>
-//         </Route>
-//       </Switch>
-//     </BrowserRouter>
-//   );
-// }
- 
-// export default App;
-
 import React from 'react';
 import FooterNew from './components/pages/FooterNew/FooterNew'
 import NavbarNew from './components/supportComponents/NavbarNew';
